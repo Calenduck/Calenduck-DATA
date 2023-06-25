b42a44ffb2bc4b5d2d35bc6bc7f998e6a7ddf0b4
공성재 <gsj7723@backpac.kr>
2023-06-25 16:32:12 +0900

    작품추천 알고리즘 init

--- src/main/scala/com/competition/saveNameWithMt20IDJob.scala
+++ src/main/scala/com/competition/saveNameWithMt20IDJob.scala
@@ -10,6 +10,18 @@ import java.util.Properties
 import java.time.LocalDate
 
 case class nameWithMt20ID(name:String, mt20id:String)
+case class detailInfo(mt20id:String
+                      ,mt10id:String
+                      ,prfnm:String
+                      ,fcltynm:String
+                      ,prfcast:String
+                      ,prfcrew:String
+                      ,prfruntime:String
+                      ,prfage:String
+                      ,entrpsnm:String
+                      ,pcseguidance:String
+                      ,genrenm:String
+                     )
 
 object saveNameWithMt20IDJob {
   val splitString = (x:String) => {
@@ -31,8 +43,8 @@ object saveNameWithMt20IDJob {
     val showInfos : DataFrame = getShowInfoJob.getShowInfo(spark, from, to)
     val mt20ids = showInfos.select("mt20id").collect.map(v=>v(0).toString)
 
-
-    var df : DataFrame = spark.emptyDataset[nameWithMt20ID].toDF()
+    var detailInfoDf : DataFrame = spark.emptyDataset[nameWithMt20ID].toDF()
+    var nameWithMt20idDf : DataFrame = spark.emptyDataset[nameWithMt20ID].toDF()
     mt20ids.foreach{mt20id=>
       val showDetail : DataFrame =
         getShowDetailJob.getShowDetail(spark, mt20id)
@@ -40,16 +52,16 @@ object saveNameWithMt20IDJob {
 
       val tmpDf = showDetail.select(explode($"names").alias("name"), col("mt20id"))
 
-      df = df.unionAll(tmpDf)
+      nameWithMt20idDf = nameWithMt20idDf.unionAll(tmpDf)
       Thread.sleep(600)
     }
 
     // Configure the connection properties
-    val jdbcHostname = "competition.cjyqslqcsafp.ap-northeast-2.rds.amazonaws.com"
+    val jdbcHostname = "competition-admin.cjyqslqcsafp.ap-northeast-2.rds.amazonaws.com"
     val jdbcPort = 3306
     val jdbcDatabase = "competition"
     val jdbcUsername = "competition"
-    val jdbcPassword = "!g794613"
+    val jdbcPassword = "!!Gg794613"
 
     val connectionProperties = new Properties()
     connectionProperties.put("user", jdbcUsername)
@@ -58,11 +70,27 @@ object saveNameWithMt20IDJob {
     val jdbcUrl = s"jdbc:mysql://$jdbcHostname:$jdbcPort/$jdbcDatabase"
 
     // Create the table if it doesn't exist
-    val tableName = "name_with_mt20id"
-    val createTableQuery =
+    var tableName = "name_with_mt20id"
+    var createTableQuery =
       s"""CREATE TABLE IF NOT EXISTS $tableName (
          |  name VARCHAR(100),
          |  mt20id VARCHAR(100)
+         |)""".stripMargin
+
+    tableName = "detail_info"
+    createTableQuery =
+      s"""CREATE TABLE IF NOT EXISTS $tableName (
+         |mt20id VARCHAR(100),
+         |mt10id VARCHAR(100),
+         |prfnm VARCHAR(100),
+         |fcltynm VARCHAR(100),
+         |prfcast VARCHAR(100),
+         |prfcrew VARCHAR(100),
+         |prfruntime VARCHAR(100),
+         |prfage VARCHAR(100),
+         |entrpsnm VARCHAR(100),
+         |pcseguidance VARCHAR(100),
+         |genrenm VARCHAR(100)
          |)""".stripMargin
 
     val connection = java.sql.DriverManager.getConnection(jdbcUrl, connectionProperties)
@@ -70,15 +98,14 @@ object saveNameWithMt20IDJob {
     statement.execute(createTableQuery)
 
     // Save the DataFrame to AWS RDS
-    df.write
+    nameWithMt20idDf.write
       .mode(SaveMode.Overwrite)
       .jdbc(jdbcUrl, tableName, connectionProperties)
 
     statement.close()
     connection.close()
 
-
-    //df.repartition(1)
+    //nameWithMt20idDf.repartition(1)
     //  .write
     //  .mode(SaveMode.Overwrite)
     //  .option("compression", "snappy")
